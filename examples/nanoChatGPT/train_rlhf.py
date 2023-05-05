71106bcb4aef772d730fdae477bd7bf23d3f5d44
vmoens <vincentmoens@gmail.com>
2023-05-05 15:57:45 +0100

    amend

--- examples/nanoChatGPT/train_rlhf.py
+++ examples/nanoChatGPT/train_rlhf.py
@@ -79,7 +79,9 @@ def main():
     for i in range(max_iters):
         with torch.no_grad():
             td = env.rollout(ep_length, policy=actor, return_contiguous=True)
-
+        print(
+            f"Iteration {i}: {loss_val=}, reward={td.get(('next', 'reward')).mean(): 4.4f}"
+        )
         for epoch in range(num_epochs):
             adv_fn(td)
             rb.extend(td.view(-1))
@@ -97,9 +99,6 @@ def main():
                 optimizer.zero_grad()
 
                 # Logging
-        print(
-            f"Iteration {i}: {loss_val=}, reward={td.get(('next', 'reward')).mean(): 4.4f}"
-        )
 
     # TODO: save model
     # TODO: generate something?
